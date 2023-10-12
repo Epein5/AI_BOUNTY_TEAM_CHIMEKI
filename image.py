4e88e19710fe360d742793d85b71ea73db8f9195
Epein5 <aayushgtm5@gmail.com>
2023-10-12 04:58:10 -0700

    Text in Images_function

--- image.py
+++ image.py
@@ -1,58 +1,45 @@
-# from PIL import Image, ImageDraw, ImageFont
-
-# # Open an image file
-# image = Image.open("mahashivaratri.jpg")
-
-# # Create a drawing object
-# draw = ImageDraw.Draw(image)
-
-# # Define the text and font
-# text = "  May the festive of lights bestow upon us #GaiTihar #FestivalOfLights"
-# font = ImageFont.truetype("arial.ttf", 15)  # Change the font and size as needed
-
-# # Define the position where you want to add the text
-# text_position = (10, 10)  # (x, y)
-
-# # Define the text color
-# text_color = (0, 0, 0)  # White color in RGB
-
-# # Add the text to the image
-# draw.text(text_position, text, fill=text_color, font=font)
-
-# # Save or display the modified image
-# image.save("output_image.jpg")  # Save the modified image to a file
-# image.show()  # Display the modified image
-
 from PIL import Image, ImageDraw, ImageFont
 import textwrap
 
-# Open an image file
 image = Image.open("mahashivaratri.jpg")
 
-# Create a drawing object
+image_width, _ = image.size
+
 draw = ImageDraw.Draw(image)
 
-# Define the text and font
 text = "उज्यालोको जीवन्त चाड तिहार मनाइँदै ! यो शुभ अवसरले सबैमा सकारात्मक आनन्द ल्याओस् । सबैलाई एक चमकदार र अविस्मरणीय उत्सवको शुभकामना! "
-font = ImageFont.truetype("nepali_font.otf", 15)  # Change the font and size as needed
+font = ImageFont.truetype("nepali_font.otf", 15)  
 
-# Define the position where you want to add the text
-text_position = (10, 10)  # (x, y)
+text_position = (10, 10) 
 
-# Define the text color
-text_color = (0, 0, 0)  # White color in RGB
+text_color = (0, 0, 0) 
 
-# Define the maximum width for text
-# image_width, _ = image.size
+max_width = image_width
 
-# max_width = image_width - text_position[0]  # Adjust to fit your image width
+wrapped_text = textwrap.fill(text, width=max_width/10) 
 
-# Wrap the text to fit within the specified width
-wrapped_text = textwrap.fill(text, width=20)  # Adjust width as needed
+draw.text(text_position, wrapped_text, fill=text_color, font=font,)
 
-# Add the wrapped text to the image
-draw.text(text_position, wrapped_text, fill=text_color, font=font)
+image.save("output_image.jpg")
 
-# Save or display the modified image
-image.save("output_image.jpg")  # Save the modified image to a file
-image.show()  # Display the modified image
+
+def add_text_to_image(img_url, text):
+    image = Image.open(img_url)
+
+    image_width, _ = image.size
+
+    draw = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype("nepali_font.otf", 15) 
+    
+    text_position = (10, 10) 
+
+    text_color = (0, 0, 0) 
+
+    max_width = image_width
+
+    wrapped_text = textwrap.fill(text, width=max_width/10)
+
+    draw.text(text_position, wrapped_text, fill=text_color, font=font,)
+
+    image.save("output_image.jpg")  
