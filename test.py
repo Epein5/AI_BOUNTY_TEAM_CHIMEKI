534100b185738d2763f2f0bbfb80b5ca417a053e
Epein5 <aayushgtm5@gmail.com>
2023-10-12 22:40:00 +0545

    Save_Image_Option and Updated bg color

--- test.py
+++ test.py
@@ -5,6 +5,8 @@ import requests
 import urllib.request
 from bs4 import BeautifulSoup
 import random
+
+
 openai.api_key = 'afssfiqwfwefebgsg39wehger9grghw0wefwe-sgdffbgdfbd ' # enter your own API key here from openai
 
 def prompttext(prompt_texxt):
@@ -51,22 +53,37 @@ def translate_english_to_neplaiese(got_message):
     return final_message
 
 def scrape_image_data(promptword):
-    print(promptword)
     url = f'https://www.123rf.com/stock-photo/{promptword}.html'
     #https://www.123rf.com/stock-photo/dashain.html
-    print(url)
+    # print(url)
     html = requests.get (url)
     soup = BeautifulSoup(html.content, 'html.parser')
     random_number = random.randint(1, 3)
-    a_name =soup.find_all(class_="ImageThumbnail__image ImageThumbnail__image--beta")[random_number]
+    a_name =soup.find_all(class_="ImageThumbnail__image ImageThumbnail__image--beta")[0]
     a_name = str(a_name)
     a = a_name.split('src=')[1]
-    img_src = a.split("""**""")[0]
-    print(img_src)
+    # print(a)
+    img_src = a.split('''"''')[1]
+    # print(img_src)
 
-                 
+    response = requests.get(img_src)
+    if response.status_code == 200:
+        with open(f'{promptword}.jpg', 'wb') as f:
+            f.write(response.content)
+            print(f"Image saved as {promptword}.jpg")
+    else:
+        print("Failed to download the image.")
+
+    return img_src
+
+
+
+def generatepost(prompttextt):
+    image_url = scrape_image_data(prompttextt)
+    text = get_text_from_ChatGPT(prompttextt)
+    pass
+
+
 if __name__ == '__main__':
-    # translate_english_to_neplaiese(get_text_from_ChatGPT(prompttext("Dashain")))
-    scrape_image_data("Nepal New Year")
-
-
+    translate_english_to_neplaiese(get_text_from_ChatGPT(prompttext("MahaShivaratri")))
+    scrape_image_data("MahaShivaratri")
