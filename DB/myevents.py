df22b493d067eb2854984fb76ee0230d800b25d4
Epein5 <aayushgtm5@gmail.com>
2023-10-14 12:40:07 +0545

    Automated-calls-for-Social-Media_post

--- DB/myevents.py
+++ DB/myevents.py
@@ -1,7 +1,5 @@
 # myevents= {}myevents["Aasyshaas Marraige"] = "13 January"
 myevents= {
-"Akanshya and Subeknshya Valentines day": "17 December",
-"Avinab Birhtday": "12 January",
-"Pranjal and Anitas Anniversary": "18 March",
+    "Pranjal and Anitas Anniversary": "18 March",
 
 }
--- test.py
+++ test.py
@@ -2,15 +2,23 @@ from tkinter import Image
 import openai
 from googletrans import Translator
 import requests
-import urllib.request
 from bs4 import BeautifulSoup
 import random
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+import numpy as np
+import json
+import os
 
+def read_api_key(filename):
+    with open(filename, 'r') as config_file:
+        config = json.load(config_file)
+        return config["api_key"]
 
-openai.api_key = 'afssfiqwfwefebgsg39wehger9grghw0wefwe-sgdffbgdfbd ' # enter your own API key here from openai
+openai.api_key = read_api_key("config.json") # Replace this with path where your config.json file is located
 
 def prompttext(prompt_texxt):
-    prompt_text = f'Just Give me one paragraph of 25 words of {prompt_texxt}  to post on social media . (should contain #s ONLY at the end with some emojis.).Just the content that i can copy and paste'
+    prompt_text = f'Give me one paragraph of 30 words exactly of {prompt_texxt}  to post on social media .Only the content that i can copy and paste (should contain #s  at the end with some emojis). The words should be In  Nepali Script'
     return prompt_text
 
 def get_image_form_DallE(prompt):
@@ -31,7 +39,7 @@ def get_text_from_ChatGPT(prompt):
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": prompt},
         ],
-        max_tokens= 27 *5,
+        max_tokens= 35 *5,
     )
     print("TEXT SUCCESSFULLY GENERATED")
     text = response.choices[0].message["content"]
@@ -59,7 +67,7 @@ def scrape_image_data(promptword):
     html = requests.get (url)
     soup = BeautifulSoup(html.content, 'html.parser')
     random_number = random.randint(1, 3)
-    a_name =soup.find_all(class_="ImageThumbnail__image ImageThumbnail__image--beta")[0]
+    a_name =soup.find_all(class_="ImageThumbnail__image ImageThumbnail__image--beta")[1]
     a_name = str(a_name)
     a = a_name.split('src=')[1]
     # print(a)
@@ -68,22 +76,68 @@ def scrape_image_data(promptword):
 
     response = requests.get(img_src)
     if response.status_code == 200:
-        with open(f'{promptword}.jpg', 'wb') as f:
+        with open(f'DB/OutputImage/{promptword}.jpg', 'wb') as f:
+            savedurl = f'DB/OutputImage/{promptword}.jpg'
             f.write(response.content)
             print(f"Image saved as {promptword}.jpg")
+    
+
     else:
         print("Failed to download the image.")
+    # print(savedurl)
+    return savedurl
 
-    return img_src
+def determine_text_color(bg_color):
+    brightness = np.mean(bg_color)
+    
+    if brightness > 128:
+        return (0, 0, 0)  # Black
+    else:
+        return (255, 255, 255)  # White
 
+def add_text_to_image(img_url, text, promptword):
+    image = Image.open(img_url)
+    image_width, image_height = image.size
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("nepali_font.otf", 15) 
+    text_position = (10, 10) 
+    bg_color = image.getpixel(text_position)
+    text_color = determine_text_color(bg_color)
+    max_width = image_width
+    wrapped_text = textwrap.fill(text, width=max_width/10)
+    draw.text(text_position, wrapped_text, fill=text_color, font=font)
+    image.save(f'DB/FinalImage/{promptword}.jpg')
+    print(f"Final image saved at: DB/FinalImage/{promptword}.jpg")
+    return f'DB/FinalImage/{promptword}.jpg'
 
 
 def generatepost(prompttextt):
-    image_url = scrape_image_data(prompttextt)
-    text = get_text_from_ChatGPT(prompttextt)
-    pass
+    scraped_image_path = scrape_image_data(prompttextt)
+    text = get_text_from_ChatGPT(prompttext(prompttextt))
+    text_without_hastag = text.split("#")[0]
+    # nepali_text_without_hastag = translate_english_to_neplaiese(text_without_hastag)
+    # print("....")
+    # nepali_text = translate_english_to_neplaiese(text)
+    final_image_path = add_text_to_image(scraped_image_path, text_without_hastag,prompttextt)
 
+    return final_image_path,text,prompttextt
+
+def saveGeneratedPostToLocalDatabse(text):
+    a = generatepost(text)
+    image_path = a[0]
+    prompttext = a[1]
+    image_name = a[2]
+    image = Image.open(image_path)
+    # txt = text
+    os.makedirs(f'DB/POST/{image_name}',exist_ok=True)
+    # os.mkdir('DB/POST/Tihar/Tihar.txt',exist_ok=True)
+    image = Image.open(image_path)
+    image.save(f"DB/POST/{image_name}/{image_name}.jpg")
+    with open(f'DB/POST/{image_name}/{image_name}.txt', 'a',encoding="utf-8") as source_file:
+        source_file.write(prompttext)
+        
+        
 
 if __name__ == '__main__':
-    translate_english_to_neplaiese(get_text_from_ChatGPT(prompttext("MahaShivaratri")))
-    scrape_image_data("MahaShivaratri")
+    # generatepost("MahaShivaratri")
+    saveGeneratedPostToLocalDatabse("BuddhaJayanti")
